73f2e37d951cc80d267c7d01f28e57719a27bb46
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-04-20 16:14:40 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,22 +8,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'AppBar Demo',
-            style: TextStyle(color: Colors.black),
-          ),
-          centerTitle: true, // ให้ข้อความอยู่กึ่งกลาง AppBar
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add_alert),
-              onPressed: null,
-            )
-          ],
-          backgroundColor: Colors.orange.shade700,
+          title: Text('Form Demo'),
         ),
-        body: Container(),
+        body: MyForm(),
+      ),
+    );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  @override
+  MyFormState createState() {
+    return MyFormState();
+  }
+}
+
+class MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.person),
+              hintText: 'Enter your name',
+              labelText: 'Name',
+            ),
+
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter your name';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.work),
+              hintText: 'Enter a Department',
+              labelText: 'Department',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter a department';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.phone),
+              hintText: 'Enter a phone number',
+              labelText: 'Phone',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter a phone number';
+              }
+              return null;
+            },
+          ),
+          new Container(
+            padding: const EdgeInsets.only(left: 150.0, top: 40.0),
+            child: new ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Data Complete'),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
